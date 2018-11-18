28c835e1703362f92ea3d256711f26a963c589ab
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-11-19 00:28:31 +0200

    [SKTools] added unity system timer

---
+++ Timers/Editor/Tests/UnitySystemTimerTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace SKTools.Base.Tests
+{
+    public class UnityTimerTest
+    {
+        [UnityTest]
+        [Timeout(10 * 1000)]
+        public IEnumerator TestCoolDownWhenInterval1000ms()
+        {
+            var countUpdated = 0;
+            var seconds = 5;
+            ushort interval = 1000;
+            var timer = new UnitySystemTimer();
+            timer.SetInterval(interval);
+            var watch = new Stopwatch();
+
+            timer.OnUpdated += delegate(TimeSpan time)
+            {
+                UnityEngine.Debug.Log(time);
+                countUpdated++;
+            };
+
+            timer.OnFinished += delegate(TimeSpan time)
+            {
+                watch.Stop();
+                Assert.IsTrue((int) watch.Elapsed.TotalSeconds == seconds);
+                Assert.IsTrue(seconds * 1000 / interval + 1 == countUpdated);
+                UnityEngine.Debug.Log(
+                    "OnFinished: " + timer.Value + " Elapsed watching: " + (int) watch.Elapsed.TotalSeconds +
+                    " countUpdate=" + countUpdated + " watch.Elapsed." + watch.Elapsed.TotalMilliseconds);
+            };
+
+            watch.Start();
+            timer.StartCooldownWithSeconds(seconds);
+            yield return new WaitWhile(() => timer.IsStarted);
+        }
+
+        [UnityTest]
+        [Timeout(10 * 1000)]
+        public IEnumerator TestCoolDownWhenInterval100ms()
+        {
+            var countUpdated = 0;
+            var seconds = 2;
+            ushort interval = 100;
+            var timer = new UnitySystemTimer();
+            timer.SetInterval(interval);
+            var watch = new Stopwatch();
+
+            timer.OnUpdated += delegate(TimeSpan time)
+            {
+                UnityEngine.Debug.Log(time);
+                countUpdated++;
+            };
+
+            timer.OnFinished += delegate(TimeSpan time)
+            {
+                watch.Stop();
+                Assert.IsTrue((int) watch.Elapsed.TotalSeconds == seconds);
+                Assert.IsTrue(seconds * 1000 / interval + 1 == countUpdated);
+                UnityEngine.Debug.Log(
+                    "OnFinished: " + timer.Value + " Elapsed watching: " + (int) watch.Elapsed.TotalSeconds +
+                    " countUpdate=" + countUpdated + " watch.Elapsed." + watch.Elapsed.TotalMilliseconds);
+            };
+
+            watch.Start();
+            timer.StartCooldownWithSeconds(seconds);
+            yield return new WaitWhile(() => timer.IsStarted);
+        }
+        
+        [UnityTest]
+        [Timeout(10 * 1000)]
+        public IEnumerator TestWhenInterval1000ms()
+        {
+            var countUpdated = 0;
+            var seconds = 5;
+            ushort interval = 1000;
+            var timer = new UnitySystemTimer();
+            timer.SetInterval(interval);
+            var watch = new Stopwatch();
+
+            timer.OnUpdated += delegate(TimeSpan time)
+            {
+                UnityEngine.Debug.Log(time);
+                countUpdated++;
+                if (countUpdated == seconds + 1)
+                {
+                    timer.Stop();
+                    Assert.IsTrue((int) timer.Value.TotalSeconds == seconds);
+                }
+            };
+
+            watch.Start();
+            timer.Start();
+            yield return new WaitWhile(() => timer.IsStarted);
+        }
+    }
+}
---
+++ Timers/UnitySystemTimer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Timer = System.Timers.Timer;
+
+namespace SKTools.Base
+{
+    public class UnitySystemTimer : IDisposable
+    {
+        private const ushort DEFAULT_INTERVAL = 1000;
+        private TimeSpan _updateInterval;
+        private Timer _systemTimer;
+        private TimeSpan _timeSpan;
+        private bool _isCooldownMode;
+
+        public event Action<TimeSpan> OnUpdated;
+        public event Action<TimeSpan> OnFinished;
+
+        /// <summary> 
+        /// Wrapper of System.Timers.Timer, DEFAULT_INTERVAL = 1000ms
+        /// </summary>
+        public UnitySystemTimer()
+        {
+            _updateInterval = TimeSpan.FromMilliseconds(DEFAULT_INTERVAL);
+            _systemTimer = new Timer { Interval = DEFAULT_INTERVAL };
+            _systemTimer.AutoReset = true;
+            _systemTimer.Enabled = false;
+        }
+
+        public bool IsStarted { get; private set; }
+
+        public TimeSpan Value
+        {
+            get { return _timeSpan; }
+        }
+
+        public void SetInterval(ushort intervalMs)
+        {
+            _systemTimer.Interval = intervalMs;
+            _updateInterval = TimeSpan.FromMilliseconds(DEFAULT_INTERVAL);
+        }
+
+        public void Start()
+        {
+            StartInternal(TimeSpan.Zero, false);
+        }
+
+        public void StartCooldownWithSeconds(int seconds)
+        {
+            StartInternal(TimeSpan.FromSeconds(seconds), true);
+        }
+
+        private void StartInternal(TimeSpan timeSpan, bool isCoolDown)
+        {
+            Assert.IsNotNull(_systemTimer, "Cannot use timer after realising");
+            Assert.IsFalse(IsStarted, "This timer is already started.");
+            Assert.IsTrue(IsUnityContex, "The unitysystemtimer is not thread safe, please call only on unity main thread");
+
+            IsStarted = true;
+            _isCooldownMode = isCoolDown;
+            _timeSpan = timeSpan;
+            _systemTimer.Elapsed += OnSystemTimerElapsed;
+            _systemTimer.Enabled = true;
+            SafeInvoke(OnUpdated, _timeSpan);
+        }
+
+        public void Stop()
+        {
+            Assert.IsNotNull(_systemTimer, "Cannot use timer after realising");
+            Assert.IsTrue(IsStarted, "This timer is not started");
+            Assert.IsTrue(IsUnityContex, "The unitysystemtimer is not thread safe, please call only on unity main thread");
+
+            IsStarted = false;
+            _systemTimer.Elapsed -= OnSystemTimerElapsed;
+            _systemTimer.Enabled = false;
+            SafeInvoke(OnFinished, _timeSpan);
+        }
+
+        public void Release()
+        {
+            if (_systemTimer != null)
+            {
+                _systemTimer.Elapsed -= OnSystemTimerElapsed;
+                _systemTimer.Enabled = false;
+                _systemTimer.Dispose();
+                _systemTimer = null;
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            Release();
+            GC.SuppressFinalize(this);
+        }
+
+        private void OnSystemTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            CheckInvokationUnityContext(TimeUpdateOnUnityMainThread);
+        }
+
+        /// <summary>
+        /// This call is synchronized with unity main thread
+        /// </summary>
+        private void TimeUpdateOnUnityMainThread()
+        {
+            if (_isCooldownMode)
+            {
+                if (_timeSpan >= _updateInterval)
+                {
+                    _timeSpan = _timeSpan.Subtract(_updateInterval);
+                    SafeInvoke(OnUpdated, _timeSpan);
+                }
+
+                if (_timeSpan < _updateInterval)
+                {
+                    Stop();
+                }
+            }
+            else
+            {
+                _timeSpan = _timeSpan.Add(_updateInterval);
+                SafeInvoke(OnUpdated, _timeSpan);
+            }
+        }
+
+        private static void SafeInvoke(Action<TimeSpan> action, TimeSpan time)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            try
+            {
+                action(time);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        ~UnitySystemTimer()
+        {
+            Release();
+        }
+
+        #region SynchronizationContext
+
+        private static SynchronizationContext _unitySynchronizationContext;
+
+        private static bool IsUnityContex
+        {
+            get { return _unitySynchronizationContext == SynchronizationContext.Current; }
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void UnitySynchronizationContextCatch()
+        {
+            if (_unitySynchronizationContext == null)
+            {
+                _unitySynchronizationContext = SynchronizationContext.Current;
+            }
+        }
+
+        private static void CheckInvokationUnityContext(Action action)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            try
+            {
+                if (IsUnityContex)
+                {
+                    action();
+                }
+                else
+                {
+                    _unitySynchronizationContext.Post(_ => action(), null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        #endregion
+    }
+}
