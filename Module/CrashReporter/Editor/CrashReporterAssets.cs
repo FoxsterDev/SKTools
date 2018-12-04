d8a4d1619c870ae649c2a60e31b393a076097a86
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-12-05 00:53:01 +0300

    [SKTools.Module.CrashReporter] initial commit

---
+++ Module/CrashReporter/Editor/CrashReporterAssets.cs
@@ -0,0 +1,12 @@
+﻿using SKTools.Base.Editor;
+using UnityEngine;
+
+namespace SKTools.Module.CrashReporter
+{
+    internal class Assets : AssetsContainer
+    {
+        public Assets(string assetsDirectory) : base(assetsDirectory)
+        {
+        }
+    }
+}
---
+++ Module/CrashReporter/Editor/CrashReporterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using SKTools.Base.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace SKTools.Module.CrashReporter
+{
+    [System.Serializable]
+    public class CrashReporterConfig
+    {
+        public CrashReporterConfig Load()
+        {
+            return this;
+        }
+    }
+}
