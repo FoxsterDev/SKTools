a005f019135b08a5424eb0b55c2f1e29aa1c81c2
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-12-09 02:54:39 +0300

    [SKTools.Base] added base editor json config

---
+++ Base/Editor/EditorJsonConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace SKTools.Base.Editor
+{
+    [System.Serializable]
+    public abstract class EditorConfig
+    {
+        protected EditorConfig()
+        {
+            
+        }
+        
+        protected EditorConfig(string json)
+        {
+            EditorJsonUtility.FromJsonOverwrite(json, this);
+        }
+
+        protected virtual string FileName
+        {
+            get { return GetType().Name + ".json"; }
+        }
+        
+        public virtual T Load<T>(string relativeFolder = "Editor Resources") where T : EditorConfig, new()
+        {
+            try
+            {
+                var filePath = Utility.GetPathRelativeToExecutableCurrentFile(relativeFolder, FileName);
+                if (File.Exists(filePath))
+                {
+                    EditorJsonUtility.FromJsonOverwrite(File.ReadAllText(filePath), this);
+                    return (T)this;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+
+            return new T();
+        }
+
+        public virtual void Save(string relativeFolder = "Editor Resources")
+        {
+            try
+            {
+                var filePath = Utility.GetPathRelativeToExecutableCurrentFile(relativeFolder, FileName);
+                File.WriteAllText(filePath, EditorJsonUtility.ToJson(this, true));
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
+}
